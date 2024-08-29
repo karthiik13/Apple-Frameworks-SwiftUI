8ef03ff8ba23e84d286917ea2f2881f55d89a085
karthiik <karthik.sudarsan@trenser.com>
2024-08-29 18:51:39 +0530

    update NavigationView to NavigationStack

--- Screens/FrameworkDetaiView/FrameworkDetailView.swift
+++ Screens/FrameworkDetaiView/FrameworkDetailView.swift
@@ -10,14 +10,11 @@ import SwiftUI
 struct FrameworkDetailView: View {
     
     var framework : Framework
-    @Binding var isShowingDetailView:Bool
     @State private var isShowingSafariView = false
     
     var body: some View {
         VStack{
-            XDismissButton(isShowingDetailView: $isShowingDetailView)
             
-            Spacer()
             
             FrameworkTitleView(framework:framework)
             
@@ -41,5 +38,5 @@ struct FrameworkDetailView: View {
 }
 
 #Preview {
-    FrameworkDetailView(framework: MockData.sampleFramework, isShowingDetailView: .constant(false) )
+    FrameworkDetailView(framework: MockData.sampleFramework )
 }
--- Screens/FrameworkGridView/FrameworkGridViewModel.swift
+++ Screens/FrameworkGridView/FrameworkGridViewModel.swift
@@ -8,11 +8,7 @@
 import SwiftUI
 
 final class FrameworkGridViewModel: ObservableObject {
-    var selectedFramework: Framework? {
-        didSet {
-            isShowingDetailView = true
-        }
-    }
+    
     
     
     @Published var isShowingDetailView = false
